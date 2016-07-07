75cfb3c812246f484978d895f4147bd9e1034113
jacktao8888 <1187259952@qq.com>
2016-07-07 16:52:50 +0800

    Update 2_translate_by_youdao.py

--- 2_translate_by_youdao.py
+++ 2_translate_by_youdao.py
@@ -14,13 +14,18 @@ data['keyfrom'] = 'fanyi.web'
 data['ue'] = 'UTF-8'
 data['action'] = 'FY_BY_CLICKBUTTON'
 data['typoResult'] = 'true'
+data = urllib.parse.urlencode(data).encode('utf-8')
 
 #o = urllib.parse.urlparse(url)
 #print (o)
 
-data = urllib.parse.urlencode(data).encode('utf-8')
+header = {}
+header['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 
-response = urllib.request.urlopen(url, data)
+req = urllib.request.Request(url, data, header)
+#response = urllib.request.urlopen(url, data)
+response = urllib.request.urlopen(req)
+
 
 html = response.read().decode('utf-8')
  
@@ -28,3 +33,4 @@ target = json.loads(html)
 
 
 print ("翻译结果：%s"%target['translateResult'][0][0]['tgt'])
+print req.headers
