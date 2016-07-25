4c64f780dc94172e20d4b0be9696a8431c2c716e
jacktao8888 <1187259952@qq.com>
2016-07-25 21:51:12 +0800

    Update 2_db.py

--- 2_db.py
+++ 2_db.py
@@ -8,3 +8,13 @@ def addUser(username,password):
   cur.execute(sql)
   cur.commit()
   cur.close()
+
+def isExisted(username,password):
+  sql = "select * from test where username='%s' and password='%s'" %(username,password)
+  cur.execute(sql)
+  result = cur.fetchall()
+  cur.close()
+  if (len(result)==0):
+    return False
+  else:
+    return True
