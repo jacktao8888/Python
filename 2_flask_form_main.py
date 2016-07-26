f519ddbcc167223ef01b6cd6bd28e88db26aac50
jacktao8888 <1187259952@qq.com>
2016-07-26 22:15:56 +0800

    Update 2_flask_form_main.py

--- 2_flask_form_main.py
+++ 2_flask_form_main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask import render_template
 from flask import redirect
+from db import *
 
 app = Flask(__name__)
 
@@ -11,17 +12,25 @@ class LoginForm(Form):
     username = TextField("username",[validators.Required()])
     password = PasswordField("password",[validators.Required()])
 
-@app.route("/user",methods=['GET','POST'])
+@app.route("/login",methods=['GET','POST'])
 def login():
     myForm = LoginForm(request.form)
     if request.method == 'POST':
-        if myForm.username.data == "jikexueyuan" and myForm.password.data == "123456" and myForm.validate():
-            return redirect("http://www.jikexueyuan.com")
+        if isExisted(myForm.username.data,myForm.password.data):
+            return "Login Successfully"
         else:
             message = "Login Failed"
             return render_template('index.html',message=message,form=myForm)
     return render_template('index.html',form=myForm)
-    
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    myForm = LoginForm(request.form)
+    if request.method == 'POST':
+        addUser(myForm.username.data,myForm.password.data)
+        return "Register Successfully"
+    return render_template('index.html',form=myForm)
+
 if __name__=='__main__':
     app.run(port=8080)
     
