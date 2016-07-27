d04a70234fdfec5bc5b0d7bea37e9f5b826c7729
jacktao8888 <1187259952@qq.com>
2016-07-27 20:48:55 +0800

    Create 0_flask_web.py

---
+++ 0_flask_web.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask import request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    return '<h1>Home</h1>'
+
+@app.route('/signin',methods=['GET'])
+def signin_home():
+    return '''<form action="/signin" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+@app.route('/signin',methods=['POST'])
+def signin():
+    if request.form['username']=='admin' and request.form['password']=='123456':
+        return '<h3>Hello,admin!</h3>'
+    return '<h3>Bad username or password</h3>'
+
+@app.route('/users/<id>')        #请求格式如：127.0.0.1:5000/users/Jerry
+def user_id(id):    
+    return 'hello '+id
+
+@app.route('/query_user')       #请求格式如：127.0.0.1:5000/query_user?id=Tom
+def query_user():
+    id = request.args.get('id')
+    return 'hello user:'+id
+    
+if __name__=='__main__':
+    app.run()             #默认5000端口
